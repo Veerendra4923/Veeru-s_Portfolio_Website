cf29a89f7d68b39715b2e2a677fc3ae18a711b7a
Mogilicharla Veerendra Sai <mogilicharlaveerendrasai@gmail.com>
2025-05-17 17:32:25 +0530

    Added multi page feature

--- Home.py
+++ Home.py
@@ -1,17 +1,27 @@
 import pandas
 import streamlit as st
 st.set_page_config(layout='wide')
-col1, col2 = st.columns(2)
+col1,col2 = st.columns([1,2])
 with col1:
     st.image("images/photo1.jpg",width=350)
 with col2:
     st.title("Mogilicharla Veerendra Sai")
+    i='''A Computer Science Graduate At Vasireddy Venkatadri International Technological University.
+    Deep Diving into PYTHON | DJANGO | ML | MLops '''
+    st.info(i)
+
     content='''Highly motivated and results-oriented professional with a strong 
     ability to learn quickly and efficiently. Adept at implementing learned skills and 
     knowledge to achieve optimal outcomes. Eager to contribute to the company's competitive edge 
     by strategically utilizing my skillset. Committed to continuous learning and improvement, 
     striving to become a key player in the company's success.'''
     st.info(content)
+    f='''Passed with percentage 75.01
+    With cummulative CGPA of 8.26.     
+    Very Communication Skills,A Good Leader,Orator
+    Open To Work.'''
+    st.info(f)
+
 c='''Below are some apps built by me . Just have a look .Feel Free to contact me!'''
 st.subheader(c)
 col3,sub_col,col4 = st.columns([1.5,0.5,1.5])
